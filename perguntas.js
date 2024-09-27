cca768e830c054d34c459e6abfb8121199be136e
github-escola-bruno <161048809+github-escola-bruno@users.noreply.github.com>
2024-09-27 12:02:56 -0300

    gbrgtb

---
+++ perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual a caqpital da França?',
+    'A capital da França é Paris'
+)
+
+
+criaCartao(
+    'Proramação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+
+criaCartao(
+    'Língua inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é HI (RAI)'
+)
