96db06021cdfde956b955e5261aaffa3b0790b52
github-escola-bruno <161048809+github-escola-bruno@users.noreply.github.com>
2024-10-18 12:00:26 -0300

    hfthd

--- perguntas.js
+++ perguntas.js
@@ -23,4 +23,14 @@ criaCartao(
     'O Bugatti Tourbillon é um hipercarro de última geração com motor V16 de 8,3 litros, 3 motores elétricos e 1.800 cv de potência. Acelera de 0 a 100 km/h em 2,0 segundos e alcança 445 km/h de velocidade máxima.'
 )
 
+criaCartao(
+    'Games',
+    'Por que o Steve é o mais forte no mundo Minecraft?',
+    'De acordo com o vídeo "O QUÃO FORTE É STEVE do MINECRAFT? Bitelo vs Balestrin", Steve é o mais forte porque "ele é o protagonista do jogo" e "tem habilidades que outros personagens não têm". Além disso, "ele é o único que pode usar todos os itens do jogo".'
+)
 
+criaCartao(
+    'Suplementação esportiva',
+    'Porque a creatina é o melhor suplemento?',
+    'A creatina é um dos melhores suplementos por: Aumento de Energia, Ganho de Massa Muscular, Melhoria na Performance, Recuperação Acelerada, Benefícios Cognitivos.'
+)
